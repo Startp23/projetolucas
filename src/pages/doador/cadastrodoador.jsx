036fc899cb61c9c46e7e42fce376f02aac71b2f1
Startp23 <138338498+Startp23@users.noreply.github.com>
2023-07-26 20:56:59 -0300

    atualização 26/07

---
+++ src/pages/doador/cadastrodoador.jsx
@@ -0,0 +1,13 @@
+import './cadastrodoador.css'
+import Form from './form'
+
+function CadastroDoador() {
+  return (
+    <>
+    <div className="fundo">
+      <Form />
+      </div>
+    </>
+  )
+}
+export default CadastroDoador
