4ff2dd9a1f8b51019aa9025a724fa15965324eda
Starp23 <projetostart8@gmail.com>
2023-08-03 21:51:10 -0300

    commit

--- src/route.jsx
+++ src/route.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Inicio from "./pages/Inicial/inicial"
 import Cadastro from './pages/doador/cadastrodoador'
@@ -16,20 +15,3 @@ export default function Router(){
     </BrowserRouter>
     )
 }
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Inicio from "./pages/Inicial/inicial"
-import Cadastro from './pages/doador/cadastrodoador'
-
-export default function Router(){
-
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Cadastro/>} />
-        <Route path='/doador' element={<Inicio/>}/>
-      </Routes>
-    </BrowserRouter>
-    )
-}
->>>>>>> b0f9aadcb3e45baf8b2c84c8fa58b2f6db5bfcf2
